1f9bcf5f4fedc3e00c9f1ce6254934d8901722aa
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-03-02 08:06:09 -0800

    axiosWithAuth and some routes

---
+++ src/utils/axiosWithAuth.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const axiosWithAuth = () => {
+    // const token = localStorage.getItem("token");
+
+    return axios.create({
+        baseURL: "https://usemytechstuff-tt26.herokuapp.com",
+        headers: {
+            // authorization: token
+        }
+    })
+}
