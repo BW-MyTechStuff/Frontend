08c629d7c70d02389138608ef46bba44fd162e61
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-03-02 19:26:05 -0800

    fetching and rendering user data/profile

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -1,11 +1,31 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-export default function IssueBoard(props) {
+export default function UserDashBoard(props) {
+
+    const [ user, setUser] = useState([])
+    const userName = localStorage.getItem("name");
+
+    useEffect(() => {
+        axiosWithAuth()
+        .get(`/users/user/name/${userName}`)
+        .then(res => {
+            console.log(userName);
+            console.log(res.data);
+            setUser(res.data);
+        })
+        .catch(err => console.log({err}))
+    }, [])
 
     return (
-        <div>
-            
+        <>
+            <div className="user-profile">
+                <h3>User Profile</h3>
+                <p>{user.fname} {user.lname}</p>
+                <p>{user.email}</p>
+                <p>{user.userrole.userroletype}</p>
+            </div>
             <button className='logout' onClick={props.logout}>Logout</button>
-        </div>
+        </>
     )
 }
