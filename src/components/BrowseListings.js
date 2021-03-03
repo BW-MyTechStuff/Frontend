7c3597003aeb7d8030f776c6ec8dfe2d13e38b0d
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-03-02 21:45:17 -0800

    browse listing update

--- src/components/BrowseListings.js
+++ src/components/BrowseListings.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from "react"
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 function BrowseListings() {
+
+    const [ items, setItems] = useState([])
+
+    useEffect(() => {
+    axiosWithAuth()
+        .get("/items/items")
+        .then(res => {
+            // console.log(res.data);
+            setItems(res.data);
+        })
+        .catch(err => console.log({err}))
+    }, [BrowseListings])
+
     return (
         <div>
-
+            {console.log(items)}
+            {items.map(item => {
+                return <div>{item.itemname}</div>
+            })}
         </div>
     )
 }
