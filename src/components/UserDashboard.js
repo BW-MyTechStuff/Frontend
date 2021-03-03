943a45713702c9bfb30fb5f40eafba21c9bd0861
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-03-02 20:38:44 -0800

    rendering owners listed items

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-export default function UserDashBoard(props) {
+export default function UserDashboard(props) {
 
     const [ user, setUser] = useState([])
     const userName = localStorage.getItem("name");
 
     useEffect(() => {
-        axiosWithAuth()
+    axiosWithAuth()
         .get(`/users/user/name/${userName}`)
         .then(res => {
-            console.log(userName);
-            console.log(res.data);
+            // console.log(userName);
+            // console.log(res.data);
             setUser(res.data);
         })
         .catch(err => console.log({err}))
@@ -29,6 +29,7 @@ export default function UserDashBoard(props) {
                     <p>{user.fname} {user.lname}</p>
                     <p>{user.email}</p>
                     {/* <p>{user.userrole.userroletype}</p> */}
+                    {/* {(user.userrole.userroletype ? user.userrole.userroletype : null)} */}
                 </div>
                 <button className='logout' onClick={props.logout}>Logout</button>
             </div>
@@ -37,6 +38,18 @@ export default function UserDashBoard(props) {
                 <Link to='/browse-listings'>
                     <p>Browse Listings</p>
                 </Link>
+            </div>
+            <br />
+            <div className="listing-container">
+                {/* <div className="currently-renting">
+                    not sure if this is possible rn
+                </div> */}
+                <div className="owner-listings">
+                    <h3>My Listed Items</h3>
+                    <Link to="my-listing-item-view">
+                        {(user.items ? user.items.map(item => {return item.itemname}) : null)}
+                    </Link>
+                </div>
             </div>
         </>
     )
