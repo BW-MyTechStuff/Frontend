3341f98db490c71e2b56e12d4570cbf81cb56fe5
Hussain Ali <ali.415code@gmail.com>
2021-03-03 19:50:27 -0800

    Completed Add Item Form & Functionality

--- src/components/AddNewItem.js
+++ src/components/AddNewItem.js
@@ -1,29 +1,69 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { useHistory } from "react-router-dom";
 
+const initialDisabled = true
+
+// working
 
 function AddNewItem() {
 
+    const userName = localStorage.getItem("name");
+    const [ user, setUser] = useState({})
+
+    useEffect(() => {                       // this data can be passed in through props instead
+    axiosWithAuth()
+        .get(`/users/user/name/${userName}`)
+        .then(res => {
+            // console.log(userName);
+            // console.log(res.data);
+            setUser(res.data);
+        })
+        .catch(err => console.log({err}))
+    }, [])
+
+    let id = user.userid;
+    let usrname = user.username;
+    let usremail = user.email
+
     const initialValues = {
-        itemName: '',
-        costPerDay:'',
-        availability: '',
-        description: '',
+        itemname: "",
+        itemcostperday: 0.00,
+        itemstatus: {
+            itemstatusid: 1,
+            itemstatustype: "Available"
+        },
+        itemdescription: "",
+        numberofdaysrented: 0
     }
-
+    
     const initialErrors = {
-        itemName: '',
-        costPerDay:'',
-        availability: '',
-        description: '',
+        itemname: "",
+        itemcostperday: 0.00,
+        itemstatus: {
+            itemstatusid: 1,
+            itemstatustype: "Available"
+        },
+        itemdescription: "",
+        numberofdaysrented: 0
     }
 
-    const initialDisabled = true
 
-    const [item, setItem] = useState({})
-    const [values, setValues] = useState(initialValues)
+    const [item, setItem] = useState(initialValues)
     const [errors, setErrors] = useState(initialErrors)
     const [disabled, setDisabled] = useState(initialDisabled)
+    const history = useHistory();
     
+    const updateForm = (name, value) => {   
+        setItem({...item, [name]:value})
+    }
+
+    const change = (evt) => {
+        const { name, value } = evt.target
+        updateForm(name, value);
+    }
+
     const postItem = newItem => {
         axios.post('https://usemytechstuff-tt26.herokuapp.com/items/item', newItem)
         .then(res => {
@@ -33,42 +73,68 @@ function AddNewItem() {
         .catch(err => {
           console.log(err);
         })
-        setValues(initialValues)
+
     }
 
-    const submit = evt => {
+
+    
+    // i would suggest using a get request to get current userr by id (u can find this in local storage) and then 
+    // set it to a variable in order to get add neceessasry info to new item obj in submit 
+
+    const addItem = evt => {
         const newItem = {
-            itemName: values.itemName,
-            costPerDay: values.costPerDay,
-            availability: values.availability,
-            description: values.description,
+            user: {
+                userid: id,
+                userrole: {
+                    userroleid: 1,
+                    userroletype: "OWNER",
+                },
+                username: usrname,
+                email: usremail,
+            },
+            itemname: item.itemname,
+            itemcostperday: item.itemcostperday,
+            itemstatus: {
+                itemstatusid: 1,
+                itemstatustype: "Available"
+            },
+            itemdescription: item.itemdescription,
+            numberofdaysrented: 0
         }
+        console.log(newItem)
         postItem(newItem)
-        setValues(initialValues)
+        
     }
 
     const cancel = evt => {
-          setValues(initialValues)
+          setItem(initialValues)
     }
+
+    const submit = e => {
+        e.preventDefault();
+        addItem()
+        history.push("/user-dashboard")
+    }
+    console.log(item)
 
     return (
         <div>
             <h1>Add New Item</h1>
             <form onSubmit = {submit}>
-                <label>
-                    <input name= 'itemName' type= 'text' onChange={change} value= {values.itemName}/>
+                <label>Name:
+                    <input name= 'itemname' type= 'text' onChange={change} value={item.itemName}/>
                 </label>
-                <label>
-                    <input name= 'costPerDay' type= 'text' onChange={change} value= {values.costPerDay}/>
+                <label>Cost per day:
+                    <input name= 'itemcostperday' type='number' step='0.01' onChange={change} value={item.costPerDay}/>
                 </label>
-                <label>
-                    <input name= 'availability' type= 'text' onChange={change} value= {values.availability}/>
+                <label>Availability:
+                    <input name= 'numberofdaysrented' type='number' onChange={change} value={item.availability}/>
                 </label>
-                <label>
-                    <input name= 'description' type= 'text' onChange={change} value= {values.description}/>
+                <label>Description:
+                    <input name= 'itemdescription' type= 'text' onChange={change} value={item.description}/>
                 </label>
-                <button onClick={cancel}> Cancel</button>
-                <button onClick={submit}>Save</button>
+                <button onClick={cancel}>Cancel</button>
+                <button onClick={submit}>Add Item</button>
             </form>
         </div>
     )
--- src/components/EditItem.js
+++ src/components/EditItem.js
@@ -2,55 +2,55 @@ import React from 'react'
 
 function EditItem() {
    
-    const initialValues = {
-        itemName: '',
-        costPerDay:'',
-        availability: '',
-        description: '',
-    }
+    // const initialValues = {
+    //     itemName: '',
+    //     costPerDay:'',
+    //     availability: '',
+    //     description: '',
+    // }
 
-    const initialErrors = {
-        itemName: '',
-        costPerDay:'',
-        availability: '',
-        description: '',
-    }
+    // const initialErrors = {
+    //     itemName: '',
+    //     costPerDay:'',
+    //     availability: '',
+    //     description: '',
+    // }
     
-    const [item, setItem] = useState({})
-    const [values, setValues] = useState(initialValues)
-    const [errors, setErrors] = useState(initialErrors)
+    // const [item, setItem] = useState({})
+    // const [values, setValues] = useState(initialValues)
+    // const [errors, setErrors] = useState(initialErrors)
         
-    const postItem = newItem => {
-        axios.post('https://usemytechstuff-tt26.herokuapp.com/items/item', newItem)
-        .then(res => {
-          setItem([...item, res.data])
-          console.log(res.data)
-        })
-        .catch(err => {
-          console.log(err);
-        })
-        setValues(initialValues)
-    }
+    // const postItem = newItem => {
+    //     axios.post('https://usemytechstuff-tt26.herokuapp.com/items/item', newItem)
+    //     .then(res => {
+    //       setItem([...item, res.data])
+    //       console.log(res.data)
+    //     })
+    //     .catch(err => {
+    //       console.log(err);
+    //     })
+    //     setValues(initialValues)
+    // }
 
-    const submit = evt => {
-        const newItem = {
-            itemName: values.itemName,
-            costPerDay: values.costPerDay,
-            availability: values.availability,
-            description: values.description,
-        }
-        postItem(newItem)
-        setValues(initialValues)
-    }
+    // const submit = evt => {
+    //     const newItem = {
+    //         itemName: values.itemName,
+    //         costPerDay: values.costPerDay,
+    //         availability: values.availability,
+    //         description: values.description,
+    //     }
+    //     postItem(newItem)
+    //     setValues(initialValues)
+    // }
 
-    const cancel = evt => {
-          setValues(initialValues)
-    }
+    // const cancel = evt => {
+    //       setValues(initialValues)
+    // }
 
     return (
         <div>
             <h1>Add New Item</h1>
-            <form onSubmit = {submit}>
+            {/* <form onSubmit = {submit}>
                 <label>
                     <input name= 'itemName' type= 'text' onChange={change} value= {values.itemName}/>
                 </label>
@@ -66,7 +66,7 @@ function EditItem() {
                 <button onClick={cancel}> Cancel</button>
                 <button onClick={submit}>Save</button>
                 <button>Delete</button>
-            </form>
+            </form> */}
         </div>
     )
 }
